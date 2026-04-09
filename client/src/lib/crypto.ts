c01305da9866149909e96b486a9acce2ae80264e
paulyimnot <paulyimnot@gmail.com>
2026-04-08 23:19:47 -0700

    Fix @noble/hashes import for build

--- client/src/lib/crypto.ts
+++ client/src/lib/crypto.ts
@@ -1,5 +1,5 @@
 import * as secp256k1 from "@noble/secp256k1";
-import { sha256 } from "@noble/hashes/sha256";
+import { sha256 } from "@noble/hashes/sha2.js";
 
 // Helper to convert hex to bytes
 function hexToBytes(hex: string): Uint8Array {
